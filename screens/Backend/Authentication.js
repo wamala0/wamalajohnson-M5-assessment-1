81cc8fcbce118ee8327147b7d25d547a24bc8aa1
wamala0 <112074839+wamala0@users.noreply.github.com>
2022-09-03 20:14:35 +0300

    Add files via upload

--- screens/Backend/Authentication.js
+++ screens/Backend/Authentication.js
@@ -1,1 +1,26 @@
 
+
+  import { watchPositionAsync } from 'expo-location';
+import * as firebase from 'firebase';
+import { Alert } from 'react-native';
+
+export async function registration (first_name,last_name,email,password){
+   await firebase.auth().createUserWithPasswordAndEmail("wamalajohnson126@gmail.comw",password);
+   const currentUser = firebase.auth().currentUser;
+
+   const db = firebase.firestore();
+   db.collection('user').doc(currentUser.uid).set({
+    email: currentUser.email,
+    password:password,
+    first_name:first_name,
+    last_name:last_name
+
+   });
+
+
+    try {
+        
+    } catch (error) {
+      Alert.alert("There's something".error)  
+    }
+}
